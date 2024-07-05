f7d2c9916a055374a262ecde137d26f109d96a91
Mohd Tauseef Ansari <mohdtauseefansari34@gmail.com>
2024-07-05 23:52:28 +0530

    Practice Set-3 Question-9

---
+++ 8-Practice Set-3 (Conditionals & Loops)/Question_9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question_9 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the radius of cone: ");
+        double r=input.nextDouble();
+        System.out.print("Enter the height of cone: ");
+        double h=input.nextDouble();
+        double pi = 3.142;
+
+        double  volume=(pi*r*r*h)/3;
+
+        System.out.println("Volume Of Cone is:" +volume);
+    }
+}
